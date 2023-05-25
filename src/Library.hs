4a6b7041c883b346565d0b19004f02e0fc7d5065
juanFdS <juan9794@gmail.com>
2023-05-25 13:51:38 -0300

    Simulacro de parcial terminado

--- src/Library.hs
+++ src/Library.hs
@@ -39,7 +39,8 @@ data Barco = Barco {
     tripulantes :: [Tripulante],
     tipoDeBarco :: TipoDeBarco,
     oro :: Number,
-    balas :: Number
+    balas :: Number,
+    madera :: Number
  } deriving (Eq, Show)
 
 data TipoDeBarco = Galeon | Bergatin | Balandro deriving (Eq, Show)
@@ -56,6 +57,9 @@ modificarCantidadBalas cantidadBalas barco = barco { balas = cantidadBalas }
 modificarCantidadOro :: Number -> Barco -> Barco
 modificarCantidadOro cantidadOro barco = barco { oro = cantidadOro }
 
+modificarCantidadMadera :: Number -> Barco -> Barco
+modificarCantidadMadera cantidadMadera barco = barco { madera = cantidadMadera }
+
 llenar :: Barco -> Barco
 llenar barco = (
     modificarCantidadOro (metrosCuadrados barco * 7) . modificarCantidadBalas (cantidadTripulantes barco * 3)
@@ -68,3 +72,181 @@ metrosCuadradosSegunTipoDeEmbarcacion :: TipoDeBarco -> Number
 metrosCuadradosSegunTipoDeEmbarcacion Galeon = 150
 metrosCuadradosSegunTipoDeEmbarcacion Bergatin = 100
 metrosCuadradosSegunTipoDeEmbarcacion Balandro = 50
+
+enfrentarseContra :: Barco -> Barco -> Barco
+enfrentarseContra barco barcoEnemigo
+    | ganaContra barco barcoEnemigo = robarRecursosDe barco barcoEnemigo
+    | otherwise = vaciarRecursos barco
+
+ganaContra :: Barco -> Barco -> Bool
+ganaContra barco barcoEnemigo =
+    (barco `esMasGrandeQue` barcoEnemigo && barco `tieneIgualOMasBalasQue` barcoEnemigo) ||
+    (barco `esDeMismoTamanioQue` barcoEnemigo && barco `tieneIgualOMasMaderaQue` barcoEnemigo) ||
+    (barco `esMasChicoQue` barcoEnemigo && barco `tieneIgualOMasTripulantesVivosQue` barcoEnemigo)
+
+-- ganaContra :: Barco -> Barco -> Bool
+-- ganaContra barco barcoEnemigo = condicionDeVictoriaSegunTamanio (tamanioRelativo barco barcoEnemigo) barco barcoEnemigo
+
+-- tamanioRelativo :: Barco -> Barco -> TamanioRelativo
+-- tamanioRelativo unBarco otroBarco
+--     | metrosCuadrados unBarco > metrosCuadrados otroBarco = MasGrande
+--     | metrosCuadrados unBarco == metrosCuadrados otroBarco = Igual
+--     | otherwise = MasChico
+
+-- condicionDeVictoriaSegunTamanio :: TamanioRelativo -> Barco -> Barco -> Bool
+-- condicionDeVictoriaSegunTamanio MasGrande = tieneIgualOMasBalasQue
+-- condicionDeVictoriaSegunTamanio Igual = tieneIgualOMasMaderaQue
+-- condicionDeVictoriaSegunTamanio MasChico = tieneIgualOMasTripulantesVivosQue
+
+-- data TamanioRelativo = MasGrande | Igual | MasChico
+
+tieneMasOIgualQue :: Ord a => (Barco -> a) -> Barco -> Barco -> Bool
+tieneMasOIgualQue criterio unBarco otroBarco =
+    criterio unBarco >= criterio otroBarco
+
+esMasChicoQue :: Barco -> Barco -> Bool
+unBarco `esMasChicoQue` otroBarco =
+    metrosCuadrados unBarco < metrosCuadrados otroBarco
+
+esMasGrandeQue :: Barco -> Barco -> Bool
+unBarco `esMasGrandeQue` otroBarco =
+    metrosCuadrados unBarco > metrosCuadrados otroBarco
+
+esDeMismoTamanioQue :: Barco -> Barco -> Bool
+unBarco `esDeMismoTamanioQue` otroBarco =
+    metrosCuadrados unBarco == metrosCuadrados otroBarco
+
+tieneIgualOMasMaderaQue :: Barco -> Barco -> Bool
+tieneIgualOMasMaderaQue = tieneMasOIgualQue madera
+
+tieneIgualOMasTripulantesVivosQue :: Barco -> Barco -> Bool
+tieneIgualOMasTripulantesVivosQue = tieneMasOIgualQue cantidadTripulantesVivos
+
+tieneIgualOMasBalasQue :: Barco -> Barco -> Bool
+tieneIgualOMasBalasQue = tieneMasOIgualQue balas
+
+contar :: (a -> Bool) -> [a] -> Number
+contar condicion = length . filter condicion
+
+cantidadTripulantesVivos :: Barco -> Number
+cantidadTripulantesVivos = contar (not . estaMuerto) . tripulantes
+
+vaciarRecursos :: Barco -> Barco
+vaciarRecursos =
+    modificarCantidadBalas 0 . modificarCantidadOro 0 . modificarCantidadMadera 0
+
+robarRecursosDe :: Barco -> Barco -> Barco
+robarRecursosDe barcoLadron barcoRobado = (
+    modificarCantidadBalas (balas barcoLadron + balas barcoRobado) .
+    modificarCantidadOro (oro barcoLadron + oro barcoRobado) .
+    modificarCantidadMadera (madera barcoLadron + madera barcoRobado)
+    ) barcoLadron
+
+-------------------------
+
+cambiarTripulantes :: [Tripulante] -> Barco -> Barco
+cambiarTripulantes nuevosTripulantes barco =
+    barco { tripulantes = nuevosTripulantes }
+
+afectarTripulantes :: (Tripulante -> Tripulante) -> Barco -> Barco
+afectarTripulantes efecto barco =
+    cambiarTripulantes (map efecto (tripulantes barco)) barco
+
+type Suceso = Barco -> Barco
+
+embarcarUnTesoro :: Number -> Suceso
+embarcarUnTesoro pesoEnOro barco = (
+    afectarTripulantes (disminuirEnergia (pesoEnOro / cantidadTripulantesVivos barco)) .
+    modificarCantidadOro (pesoEnOro + oro barco)
+    ) barco
+
+enfrentarUnBarco :: Barco -> Suceso
+enfrentarUnBarco = flip enfrentarseContra
+
+afectarVariasVeces :: Number -> (Tripulante -> Tripulante) -> Tripulante -> Tripulante
+afectarVariasVeces cantidad efecto tripulante = foldr ($) tripulante (replicate cantidad efecto)
+
+beberVariasVecesGrog :: Number -> Tripulante -> Tripulante
+beberVariasVecesGrog cantidad = afectarVariasVeces 5 beberGrog
+
+-- beberVariasVecesGrog 0 tripulante = tripulante
+-- beberVariasVecesGrog n tripulante = beberVariasVecesGrog (n-1) (beberGrog tripulante)
+
+-- beberVariasVecesGrog cantidad = foldr (.) id (replicate cantidad beberGrog)
+
+encontrarCargamentoGrog :: Suceso
+encontrarCargamentoGrog = afectarTripulantes (beberVariasVecesGrog 5)
+
+estaVivo :: Tripulante -> Bool
+estaVivo = not . estaMuerto
+
+enfrentarEjercitoDeEsqueletos :: Number -> Suceso
+enfrentarEjercitoDeEsqueletos cantidad =
+    afectarPrimerTripulanteQueCumple estaVivo (enfrentarEsqueletos cantidad)
+
+enfrentarEsqueletos :: Number -> Tripulante -> Tripulante
+enfrentarEsqueletos cantidad =  afectarVariasVeces cantidad enfrentarEsqueleto
+
+afectarPrimerTripulanteQueCumple :: (Tripulante -> Bool) -> (Tripulante -> Tripulante) -> Barco -> Barco
+afectarPrimerTripulanteQueCumple condicion efecto barco = (
+        agregarTripulante ((efecto . encontrarPrimero condicion) barco) .
+        sacarTripulante (encontrarPrimero condicion barco)
+    ) barco
+
+agregarTripulante :: Tripulante -> Barco -> Barco
+agregarTripulante tripulante barco = cambiarTripulantes (tripulante : tripulantes barco) barco
+
+encontrarPrimero :: (Tripulante -> Bool) -> Barco -> Tripulante
+encontrarPrimero condicion = head . filter condicion . tripulantes
+
+primerTripulanteConVida :: Barco -> Tripulante
+primerTripulanteConVida = head . filter (not . estaMuerto) . tripulantes
+
+sacarTripulante :: Tripulante -> Barco -> Barco
+sacarTripulante tripulante barco = cambiarTripulantes (sacar tripulante (tripulantes barco)) barco
+
+sacar :: Eq a => a -> [a] -> [a]
+sacar elemento lista = takeWhile (/= elemento) lista ++ tail (dropWhile (/= elemento) lista)
+
+precioGrog = 30
+
+estanTodosVivos :: Barco -> Bool
+estanTodosVivos = all (not . estaMuerto) . tripulantes
+
+primerTripulanteMuerto :: Barco -> Tripulante
+primerTripulanteMuerto = head . filter estaMuerto . tripulantes
+
+pasarPorTiendaDeGrog :: Barco -> Barco
+pasarPorTiendaDeGrog barco
+    | oro barco < precioGrog || estanTodosVivos barco = barco
+    | otherwise = (afectarPrimerTripulanteQueCumple estaMuerto beberGrog . modificarCantidadOro (oro barco - precioGrog)) barco
+
+suceder :: Barco -> (Barco -> Barco) -> Barco
+suceder barco suceso
+    | esBarcoFantasma barco = barco
+    | otherwise = suceso barco
+
+data Travesia = Travesia { recompensaEnOro :: (Barco -> Number), sucesos :: [Suceso] }
+
+fuerteDeLosCondenados :: Travesia
+fuerteDeLosCondenados = Travesia (\_ -> 50) [enfrentarEjercitoDeEsqueletos 100, pasarPorTiendaDeGrog, embarcarUnTesoro 30]
+
+travesiaDelFlameHeart :: Travesia
+travesiaDelFlameHeart = Travesia ((*200) . cantidadTripulantesVivos) [
+    enfrentarUnBarco (Barco (replicate 4 (Tripulante 30)) Galeon 0 50 50),
+    enfrentarUnBarco (Barco (replicate 3 (Tripulante 10)) Bergatin 0 30 30),
+    encontrarCargamentoGrog,
+    embarcarUnTesoro 150
+    ]
+
+laGirita :: Travesia
+laGirita = Travesia (\barco -> oro barco) (replicate 4 pasarPorTiendaDeGrog ++ [enfrentarEjercitoDeEsqueletos 10])
+
+realizarTravesia :: Travesia -> Barco -> Barco
+realizarTravesia travesia barco =
+    (cobrarRecompensaSiSobrevivio (recompensaEnOro travesia) . foldl suceder barco) (sucesos travesia)
+
+cobrarRecompensaSiSobrevivio :: (Barco -> Number) -> Barco -> Barco
+cobrarRecompensaSiSobrevivio cuantoOro barco
+    | esBarcoFantasma barco = barco
+    | otherwise = modificarCantidadOro (oro barco + cuantoOro barco) barco
--- src/Spec.hs
+++ src/Spec.hs
@@ -9,6 +9,9 @@ correrTests = hspec $ do
     describe "Simulacro de Parcial" $ do
         testsPunto1
         testsPunto2
+        testsPunto3
+        testsPunto4
+        testsPunto5
 
 testsPunto1 = do
         describe "actividades diarias" $ do
@@ -31,13 +34,17 @@ testsPunto1 = do
             it "un tripulante no esta muerto si su energia es mayor a 0" $ do
                 estaMuerto (Tripulante 1) `shouldBe` False
 
+barcoConOroYTripulantes :: [Tripulante] -> Number -> Barco
+barcoConOroYTripulantes unosTripulantes cantidadOro =
+    Barco unosTripulantes Bergatin cantidadOro 0 0
+
 barcoConTripulantes :: [Tripulante] -> Barco
 barcoConTripulantes unosTripulantes =
-    Barco unosTripulantes Bergatin 0 0
+    Barco unosTripulantes Bergatin 0 0 0
 
 barcoDeTipo :: TipoDeBarco -> Barco
 barcoDeTipo unTipo =
-    Barco [] unTipo 0 0
+    Barco [] unTipo 0 0 0
 
 testsPunto2 = do
     describe "barcos" $ do
@@ -50,7 +57,7 @@ testsPunto2 = do
                 esBarcoFantasma (barcoConTripulantes [Tripulante 0, Tripulante 0]) `shouldBe` True
         describe "llenar un barco" $ do
             it "al llenarlo, queda con 7kg de oro por m2 y 3 balas x tripulante" $ do
-                let barcoLleno = llenar (Barco [Tripulante 10, Tripulante 0] Balandro 0 0)
+                let barcoLleno = llenar (Barco [Tripulante 10, Tripulante 0] Balandro 0 0 0)
                 balas barcoLleno `shouldBe` 6
                 oro barcoLleno `shouldBe` 350
         describe "m2 del barco" $ do
@@ -60,3 +67,99 @@ testsPunto2 = do
                 metrosCuadrados (barcoDeTipo Bergatin) `shouldBe` 100
             it "si es un balandro es 50" $ do
                 metrosCuadrados (barcoDeTipo Balandro) `shouldBe` 50
+
+testsPunto3 = do
+    describe "enfrentamientos" $ do
+        it "si un barco pierde contra un enemigo, se queda sin recursos" $ do
+            let barcoDerrotado = enfrentarseContra (Barco [Tripulante 1] Galeon 10 3 7) (Barco [Tripulante 1] Bergatin 0 5 0)
+            oro barcoDerrotado `shouldBe` 0
+            balas barcoDerrotado `shouldBe` 0
+            madera barcoDerrotado `shouldBe` 0
+        it "si un barco gana contra un enemigo, se lleva los recursos del otro barco" $ do
+            let barcoGanador = enfrentarseContra (Barco [Tripulante 1] Bergatin 2 5 1) (Barco [Tripulante 1] Galeon 10 3 7)
+            oro barcoGanador `shouldBe` 12
+            balas barcoGanador `shouldBe` 8
+            madera barcoGanador `shouldBe` 8
+        describe "ganaContra" $ do
+            describe "si es mas grande que su enemigo" $ do
+                let barcoGrandeConBalas cantidadBalas = Barco [] Galeon 0 cantidadBalas 0
+                    barcoChicoConBalas cantidadBalas = Barco [] Balandro 0 cantidadBalas 0
+                it "pierde si tiene menos balas" $ do
+                    ganaContra (barcoGrandeConBalas 3)
+                               (barcoChicoConBalas 5) `shouldBe` False
+                it "gana si tiene igual cantidad de balas" $ do
+                    ganaContra (barcoGrandeConBalas 5)
+                               (barcoChicoConBalas 5) `shouldBe` True
+                it "gana si tiene mas balas" $ do
+                    ganaContra (barcoGrandeConBalas 7)
+                               (barcoChicoConBalas 5) `shouldBe` True
+            describe "si tienen el mismo tamaño" $ do
+                let barcoMedianoConMadera cantidadMadera = Barco [] Bergatin 0 0 cantidadMadera
+                it "gana si tiene mas madera" $ do
+                    ganaContra (barcoMedianoConMadera 6)
+                               (barcoMedianoConMadera 5) `shouldBe` True
+                it "gana si tiene igual madera" $ do
+                    ganaContra (barcoMedianoConMadera 5)
+                               (barcoMedianoConMadera 5) `shouldBe` True
+                it "pierde si tiene menos madera" $ do
+                    ganaContra (barcoMedianoConMadera 5)
+                               (barcoMedianoConMadera 6) `shouldBe` False
+            describe "si es mas chico que su enemigo" $ do
+                let barcoChicoConTripulantesVivos cantidadTripulantes = Barco (replicate cantidadTripulantes tripulanteVivo) Balandro 0 0 0
+                    barcoGrandeConTripulantesVivos cantidadTripulantes = Barco (replicate cantidadTripulantes tripulanteVivo) Galeon 0 0 0
+                    tripulanteVivo = Tripulante 1
+                it "le gana si tiene igual tripulantes con vida" $ do
+                    ganaContra (barcoChicoConTripulantesVivos 3)
+                                (barcoGrandeConTripulantesVivos 3) `shouldBe` True
+                it "le gana si tiene mas tripulantes con vida" $ do
+                    ganaContra (barcoChicoConTripulantesVivos 4)
+                                (barcoGrandeConTripulantesVivos 3) `shouldBe` True
+                it "pierde si tiene menos tripulantes con vida" $ do
+                    ganaContra (barcoChicoConTripulantesVivos 2)
+                                (barcoGrandeConTripulantesVivos 3) `shouldBe` False
+
+testsPunto4 = do
+    describe "Sucesos" $ do
+        describe "embarcar un tesoro" $ do
+            it "cada tripulante con vida carga una parte equivalente del peso en oro del tesoro, y se agrega al oro del barco el oro del tesoro" $ do
+                let barco = barcoConOroYTripulantes [Tripulante 10, Tripulante 6, Tripulante 0] 5
+                    barcoTrasSuceso = embarcarUnTesoro 10 barco
+                oro barcoTrasSuceso `shouldBe` 15
+                tripulantes barcoTrasSuceso `shouldBe` [Tripulante 5, Tripulante 1, Tripulante 0]
+        describe "encontrar cargamento de grogs" $ do
+            it "cada tripulante se toma 5 grogs" $ do
+                let barcoTrasSuceso = encontrarCargamentoGrog (barcoConTripulantes [Tripulante 0, Tripulante 10, Tripulante 215])
+                tripulantes barcoTrasSuceso `shouldBe` [Tripulante 100, Tripulante 110, Tripulante 315]
+        describe "enfrentar ejercito de esqueletos" $ do
+            it "el primer tripulante con vida del barco se enfrenta a todos los esqueletos uno tras otro" $ do
+                let barco = barcoConTripulantes [Tripulante 0, Tripulante 50]
+                    barcoTrasSuceso = enfrentarEjercitoDeEsqueletos 2 barco
+                tripulantes barcoTrasSuceso `shouldBe` [Tripulante 20, Tripulante 0]
+        describe "pasar por tienda de grog" $ do
+            it "si no tiene oro no pasa nada" $ do
+                let barco = barcoConOroYTripulantes [Tripulante 0, Tripulante 10] 0
+                pasarPorTiendaDeGrog barco `shouldBe` barco
+            it "si toda la tripulacion esta viva no pasa nada" $ do
+                let barco = barcoConOroYTripulantes [Tripulante 10] 10
+                pasarPorTiendaDeGrog barco `shouldBe` barco
+            it "si tiene algun tripulante muerto y tiene oro, le da de beber grog a su primer tripulante muerto y paga el costo de la bebida (30 oro)" $ do
+                let barco = barcoConOroYTripulantes [Tripulante 0, Tripulante 10] 40
+                    barcoTrasSuceso = pasarPorTiendaDeGrog barco
+                oro barcoTrasSuceso `shouldBe` 10
+                tripulantes barcoTrasSuceso `shouldBe` [Tripulante 20, Tripulante 10]
+        it "un barco fantasma no le puede ocurrir un suceso" $ do
+            let barco = barcoConOroYTripulantes [Tripulante 0] 100
+            suceder barco pasarPorTiendaDeGrog `shouldBe` barco
+
+testsPunto5 = do
+    describe "Travesias" $ do
+        it "si un barco supera la travesia (pasa los sucesos de la misma uno tras otro) sin convertirse en un barco fantasma, cobra el oro de la misma" $ do
+            let barco = barcoConOroYTripulantes [Tripulante 0, Tripulante 2000] 30
+                barcoTrasTravesia = realizarTravesia fuerteDeLosCondenados barco
+            tripulantes barcoTrasTravesia `shouldBe` [Tripulante 5, Tripulante 985]
+            oro barcoTrasTravesia `shouldBe` 80
+        it "si un barco queda fantasma tras realizar una travesia, no cobra la recompensa" $ do
+            let barco = barcoConOroYTripulantes [Tripulante 500] 25
+                barcoTrasTravesia = realizarTravesia fuerteDeLosCondenados barco
+            tripulantes barcoTrasTravesia `shouldBe` [Tripulante 0]
+            oro barcoTrasTravesia `shouldBe` 25
